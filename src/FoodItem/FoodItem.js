732c8d94ac9de62c56655ada9b81d472e9a482c4
Alexys Jordan <alexyskjordan@gmail.com>
2024-03-06 17:03:10 -0700

    Add env file

--- src/FoodItem/FoodItem.js
+++ src/FoodItem/FoodItem.js
@@ -1,10 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { useParams } from 'react-router-dom';
 import './FoodItem.css';
 
-const FoodItem = ({ foods }) => {
+const FoodItem = () => {
+
+  
   const { id } = useParams();
-  const food = foods.find((food) => food.foodId === id);
+  // You may need to handle cases where the food data is not available
+  const food = {}; // Initialize as empty object or handle it as per your requirement
 
   if (!food) {
     return <div>Food not found</div>;
