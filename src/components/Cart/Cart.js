befa42a8a5918deb9de54f5703ca56541fa53289
Alexys Jordan <alexyskjordan@gmail.com>
2024-03-09 15:25:36 -0700

    Add confirmation message

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,15 +4,20 @@ import './Cart.css';
 import { NavLink } from 'react-router-dom';
 
 export const Cart = ({ selected, deleteSelected, clearCart }) => {
-    console.log('HELLO', selected)
+    const calculateTotalQuantity = selected.reduce((accumulator, currentItem) => {
+        return accumulator + currentItem.quantity;
+      }, 0);
+    
     const checkOut = () => {
         if(selected.length === 0) {
+            // window.confirm("Are you sure you want to proceed?");
                 alert('There are no items in your cart. Search for what you want to purchase and items will be added.') 
         } else {
-            alert('look at all this food!')
+            alert(`Thank you. You purchased ${calculateTotalQuantity} items!`)
             clearCart()
         }
     }
+   
   return (
     <div className="cart-container">
       <h2>This is your Cart</h2>
@@ -24,6 +29,7 @@ export const Cart = ({ selected, deleteSelected, clearCart }) => {
             </>
         ))}
       </div>
+      <h2>Total quantity {calculateTotalQuantity}</h2>
       <NavLink to="/" >
         <button onClick={checkOut}>Checkout</button>
       </NavLink>
