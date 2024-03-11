756fa97cc52232744aa6454bd0651e5eeb37fa96
Alexys Jordan <alexyskjordan@gmail.com>
2024-03-10 19:32:22 -0600

    Add Final clean up

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import './Home.css';
 
-// export const Home = ({ food }) => {
   export const Home = () => {
-
-  return (
-    <div className="home-container">
-      <h2>Welcome to your friendly online grocery store</h2>
-      <h3>Search for the food you're looking for and add to your cart as you shop!</h3>
-  
-    </div>
-  );
-};
+    return (
+      <div className="home-container">
+        <h2>Welcome to your friendly online grocery store</h2>
+        <h3>Search for the food you're looking for and add to your cart as you shop!</h3>
+      </div>
+    );
+  };
